fd6e2a3f40c7ad1eac56425022669bf7934a7569
subake <alexkhorin199932@gmail.com>
2021-11-15 00:41:03 +0300

    Minor changes

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -4,23 +4,23 @@ from flask import render_template, Blueprint, request, redirect
 main_blueprint = Blueprint("main", __name__, template_folder='templates')
 
 
-# endpoint for monitoring all job status
+# endpoint for main store page
 @main_blueprint.route("/", methods=["GET"])
 def home():
     lst = item_list()
     return render_template('index.html', sell_flag=0, purchase_flag=0, items=lst)
 
 
-# endpoint for adding job
+# endpoint for adding item info
 @main_blueprint.route("/add_item", methods=["GET"])
 def add_new_item():
     lst = item_list()
     return render_template('index.html', sell_flag=1, purchase_flag=0, items=lst)
 
 
-# endpoint for adding job
-@main_blueprint.route("/add_wait_job", methods=["POST", "GET"])
-def add_wait_job():
+# endpoint for putting item for sale
+@main_blueprint.route("/put_on_sale", methods=["POST", "GET"])
+def put_on_sale():
     item = {
         "status": 'for_sale',
         "category": request.form['item-type'],
@@ -35,10 +35,9 @@ def add_wait_job():
     return redirect('/')
 
 
-# endpoint for deleting a job
+# endpoint for contacting item seller
 @main_blueprint.route('/purchase/<item_id>', methods=["GET"])
 def purchase_item(item_id):
-    # get job from connected redis queue
-    l = get_item(item_id)
-    # redirect to job list page
-    return render_template('index.html', sell_flag=0, purchase_flag=1, items=l)
+    lst = get_item(item_id)
+
+    return render_template('index.html', sell_flag=0, purchase_flag=1, items=lst)
