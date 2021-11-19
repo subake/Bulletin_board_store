00ec5bb2143d9e9be0c37695f12c615bc6e3cd68
subake <alexkhorin199932@gmail.com>
2021-11-19 14:12:32 +0300

    Solved race condition for put_item()

--- project/server/main/redis_funcs.py
+++ project/server/main/redis_funcs.py
@@ -1,17 +1,24 @@
 import json
 import redis
 from server import config
+from threading import Lock
 
 # get redis connection and add 'size' to generate item keys
 redis_connection = redis.from_url(config.Config.REDIS_URL)
 if 'size' not in redis_connection.scan_iter("*"):
     redis_connection.set('size', '0')
 
+lock = Lock()
+
 
 # add new item to redis
 def put_item(item):
+    lock.acquire()
+
     db_size = redis_connection.get('size').decode('utf-8')
     redis_connection.set('size', int(db_size) + 1)
+
+    lock.release()
 
     item["id"] = 'item' + db_size
 
@@ -33,4 +40,5 @@ def item_list():
 def get_item(item_id):
     item = json.loads(redis_connection.get(item_id))
     item["mail_link"] = 'mailto:' + item["mail"]
-    return [item]
+
+    return item
